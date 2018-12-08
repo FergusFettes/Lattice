0f65118ea592dbd4d21da4bc6f7dcb7ae7a849a4
Fergus <fergusfettes@gmail.com>
2018-12-08 21:22:42 +0530

    some more ising work

--- Ising/Ising.py
+++ Ising/Ising.py
@@ -14,9 +14,16 @@ def Init(allUp, N):
 
 def MonteCarloUpdate(A, nSteps):
     for _ in range(nSteps):
+        a = ra.random() * N
+        b = ra.random() * N
+        nb = A[a][b] * ( A[(a + 1)%N][b] + A[(a - 1)%N][b] + A[a][(b + 1)%N] + A[a][(b - 1)%N]
+        if nb <= 0 or ra.random() < cost[int(nb/4)]:
+            A[a][b]=-A[a][b]
+    return A
 
 beta = 0.2
 cost = np.zeros(3)
 cost[1] = np.exp(-4*beta)
 cost[2] = cost[1]*cost[1]
-IsingArray = Init(0, 10)
+N=10
+IsingArray = Init(0, N)

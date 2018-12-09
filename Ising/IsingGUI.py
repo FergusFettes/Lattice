3410bba36644f0842b0f1654f787274b4839bc26
Fergus <fergusfettes@gmail.com>
2018-12-09 22:48:28 +0530

    baby steps

---
+++ Ising/IsingGUI.py
@@ -0,0 +1,22 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+from MainWindow import Ui_MainWindow
+
+import os
+import random
+import types
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    app = QApplication([])
+    window = MainWindow()

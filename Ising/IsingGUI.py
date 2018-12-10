c7eab9e49bac832b4b905b9cf301cbd53e4a31c6
Fergus <fergusfettes@gmail.com>
2018-12-10 18:38:35 +0530

    some actual progress on the GUI

--- Ising/IsingGUI.py
+++ Ising/IsingGUI.py
@@ -2,21 +2,39 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-from MainWindow import Ui_MainWindow
-
-import os
-import random
-import types
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
+    def exit_button_clicked():
+        QCoreApplication.instance().quit()
 
     app = QApplication([])
-    window = MainWindow()
+
+    N = 100
+    primary_color = QColor(Qt.black)
+
+    canvas = QLabel()
+    canvas.setPixmap(QPixmap(N,N))
+    canvas.pixmap().fill(primary_color)
+
+    button1 = QPushButton('These')
+    button2 = QPushButton('buttons')
+    button3 = QPushButton('do')
+    button4 = QPushButton('nothing!')
+    exit_button = QPushButton('EXIT!')
+    exit_button.clicked.connect(exit_button_clicked)
+
+    vb = QVBoxLayout()
+    vb.addWidget(button1)
+    vb.addWidget(button2)
+    vb.addWidget(button3)
+    vb.addWidget(button4)
+    vb.addWidget(exit_button)
+
+    hb = QHBoxLayout()
+    hb.addLayout(vb)
+    hb.addWidget(canvas)
+
+    w = QWidget()
+    w.setLayout(hb)
+
+    w.show()
+    app.exec()

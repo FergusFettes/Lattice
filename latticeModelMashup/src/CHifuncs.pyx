d3c7061aca183c212961e253bbd57c0fb77d01a7
Fergus <fergusfettes@gmail.com>
2019-02-02 01:25:08 +0530

    emptying __init__ in main folder, hopefully this will make imports work better

---
+++ latticeModelMashup/src/CHifuncs.pyx
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cython
+
+import array
+import numpy as np
+
+from libc.stdlib cimport rand, RAND_MAX
+from src.Cfuncs cimport *
+from cpython cimport array
+cimport numpy as np

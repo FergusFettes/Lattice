c151e821d15b2bf73a3b067dcd7d911b1a9ff46a
Fergus <fergusfettes@gmail.com>
2019-02-17 15:34:17 +0530

    --alles working nicely, ready for rebuild into qt or so

--- Xupdatertest.py
+++ Xupdatertest.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 
 import src.Cfuncs as cf
+import src.Cyarr as cy
 from src.Pfuncs import (
     center_of_mass_P
 )
@@ -14,28 +15,28 @@ screendim1 = [38, 149]
 screendim2 = [45, 192]
 screendim3 = [57, 268]
 screendim4 = [77, 337]
+lampdim = [77, 50]
 
 
 beta = 1/8
 updates = 100
 threshold = 0
+cov = 0.3
 rules = np.array([[2, 3, 3, 3], [2, 3, 3, 3]], np.intc)
-bounds = array.array('i', [1, 1, 1, 1])
+bounds = array.array('i', [0, -1, 0, -1])
 bars = np.array([
                 [0, 4, 1, 0, 1, -1],
                 [0, 4, 1, 1, 1, -1],
                 ], np.intc)
 fuzz = np.array([
-                [0, 4, 1, 0, 1, -2],
+                [70, 7, 0, 0, 1, 1],
                 [0, 4, 1, 1, 1, -2],
                 ], np.intc)
 
 if __name__ == "__main__":
-
     head_position, tail_position, buffer_length, buffer_status,\
-        dim_t, arr_t, buf_t, dim_h, arr_h, buf_h = cf.init(screendim2)
+        dim_t, arr_t, buf_t, dim_h, arr_h, buf_h = cf.init(lampdim)
 
-    cov = 0.3
     cf.basic_update_buffer(
                     updates, beta, threshold, cov,
                     rules, head_position,
@@ -45,16 +46,8 @@ if __name__ == "__main__":
                  )
     arr_h = cf.update_array_positions(head_position, buffer_length, buffer_status, buf_h, 0)
 
-    fuzz = np.array([
-                    [0, 4, 1, 0, 1, -1],
-                    [0, 4, 1, 1, 1, -1],
-                    [10, 4, 1, 0, 1, 1],
-                    [10, 4, 1, 1, 1, 1],
-                    ], np.intc)
-
     changes = 0
-    frame_orig = array.array('i', [0])
-    frame = memoryview(frame_orig)
+    frame = memoryview(array.array('i', [0]))
     while True:
         # analysis here
         cf.basic_print(dim_t, arr_t, cov, bounds, bars, fuzz)
@@ -72,4 +65,5 @@ if __name__ == "__main__":
                     )
         arr_h = cf.update_array_positions(head_position, buffer_length, buffer_status, buf_h, 1)
         cf.scroll_instruction_update(fuzz, dim_t)
+        cy.roll_rows_pointer(-1, dim_h, arr_h)
         frame[0] += 1

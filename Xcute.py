8050fda165bd4d4dbb4a42b2c391f53e20858cb3
Fergus <fergusfettes@gmail.com>
2019-02-19 14:39:38 +0530

    --working on the new minimal gui

--- Xcute.py
+++ Xcute.py
@@ -4,10 +4,41 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-from GUI import MainWindow
 import random as ra
 from munch import *
 from yaml import safe_load
+
+
+
+# Draws the main window and contains the simulation code
+class MainWindow(QMainWindow):
+
+    def __init__(self, st):
+        super().__init__()
+
+        self.st = st
+        self.canvas = QLabel()
+        self.canvas.primaryColor = QColor(st.canvas.colorlist[0])
+        self.canvas.colorList = []
+        self.canvas.setPixmap(QPixmap(self.st.canvas.dim[0] * self.st.canvas.scale,
+                                self.st.canvas.dim[1] * self.st.canvas.scale))
+        self.canvas.pixmap().fill(self.primaryColor)
+
+#========================================================CanvasDock
+        #Wrap that all up in a DockWidget
+        self.CanvasDock = QDockWidget()
+        self.buts = {'canvas': self.CanvasDock}
+        self.CanvasDock.setWidget(self.canvas)
+        self.setCentralWidget(self.CanvasDock)
+#............................................................
+
+        self.show()
+        self.setWindowRole('popup')
+
+    def paint(self, image):
+        self.canvas.setPixmap(image)
+        self.canvas.repaint()
+
 
 if __name__ == '__main__':
     colHex1 = int(ra.random() * int('0xffffffff', 16))
@@ -24,7 +55,8 @@ if __name__ == '__main__':
         st = munchify(yaml['defaults'])
 
 
-    st.canvas.colorlist[0:4] = [colHex1, colHex2, colHex3, colHex4]
+    canvas = Canvas()
+    canvas.colorlist[0:4] = [colHex1, colHex2, colHex3, colHex4]
     app = QApplication([])
     w = MainWindow(st)
     app.exec()

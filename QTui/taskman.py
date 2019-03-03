2a2adb0e221a3504feaa0fc9c39481f652ff3358
Fergus <fergusfettes@gmail.com>
2019-03-03 20:37:27 +0530

    -- about ready to fire her up again (tentative)

--- QTui/taskman.py
+++ QTui/taskman.py
@@ -12,6 +12,10 @@ import time
 # Also inherits the array handler so it can send and recieve arrays.
 # All signals come in and out of this guy.
 class RunController(QObject):
+    popSig = pyqtSignal(np.ndarray, str)
+    radSig = pyqtSignal(np.ndarray, str)
+    imageSig = pyqtSignal(QPixmap)
+    canvasfpsSig = pyqtSignal(float)
     frameSig = pyqtSignal(int)
     finished = pyqtSignal()
     error = pyqtSignal(str)
@@ -22,6 +26,15 @@ class RunController(QObject):
         self.st = st
 
         self.st.general.rundone = 0
+        self.colorList = st.canvas.colorlist
+        self.wolfram_color_offset = 2
+        self.resize_image(st.canvas.dim)
+
+        self.scale = st.canvas.scale
+        self.fpsRoll = np.zeros(9, float)
+        self.st = st
+
+        self.savecount = 0
 
         self.dim = array.array('i', st.canvas.dim)
         self.buf_len = 10
@@ -34,120 +47,90 @@ class RunController(QObject):
         self.arr_h = self.buf[self.head_position[0] % self.buf_len]
         self.arr_t = self.buf[self.tail_position[0] % self.buf_len]
 
-        cy.clear_array(self.dim, self.arr_h)
-        cf.advance_array(self.head_position, self.buf_len, self.buf_h)
-        self.arr_h = cf.update_array_positions(self.head_position, self.buf_len,
-                                               self.buf_stat, self.buf, 0)
-        self.buf_stat[0] = 2 #placing the tail
+        # Process a few frames on startup
+        for 1 in range(3):
+            cy.clear_array(self.dim, self.arr_h)
+            cf.advance_array(self.head_position, self.buf_len, self.buf_h)
+            self.arr_h = cf.update_array_positions(self.head_position, self.buf_len,
+                                                self.buf_stat, self.buf, 0)
+        self.buf_stat[1] = 2 #placing the tail
         self.fpsRoll = np.zeros(9, float)
 
 #===============MAIN PROCESS OF THE THREAD===================#
     def process(self):
         QCoreApplication.processEvents()
-        if self.st.general.rundone == 0:
-            self.init_new_process()
-        else:
-            self.next_frame()
+        while True:
+            now = time.time()
+            cy.roll_rows_pointer(-1, kwargs['dim_h'], kwargs['arr_h'])
+            cf.basic_update_buffer(
+                kwargs['updates'],
+                kwargs['beta'],
+                kwargs['threshold'],
+                kwargs['rules'],
+                kwargs['head_position'],
+                kwargs['buffer_length'],
+                kwargs['dim'],
+                kwargs['arr_h'],
+                kwargs['buf'],
+                kwargs['bounds'],
+                kwargs['bars'],
+                kwargs['fuzz'],
+            )
+            kwargs['arr_h'] = cf.update_array_positions(
+                kwargs['head_position'],
+                kwargs['buffer_length'],
+                kwargs['buffer_status'],
+                kwargs['buf'],
+                0
+            )
+            cy.set_bounds(bounds[0], bounds[1], bounds[2], bounds[3], dim, arr)
+            cy.scroll_bars(dim, arr, bars)
+            scroll_noise(dim, arr, fuzz)
 
-    def init_new_process(self):
-        frame1 = self.prepare_frame()
+            arr_t_old = self.buf[(self.tail_position[0] - 1) % self.buf_len]
+            b, d = pf.get_births_deaths_P(arr_t_old, arr_t)
+            self.replace_image_positions(self, b, 0)
+            self.replace_image_positions(self, d, 0)
+            self.send_image()
 
-        self.st.general.rundone += 1
-        frame2 = self.prepare_frame()
-        self.st.general.rundone += 1
-        self.handlerinitSig.emit(frame1, frame2, self.st.canvas.dim)
-        self.frame = self.prepare_frame()
-        self.frametime = time.time()
+            cf.scroll_instruction_update(
+                kwargs['bars'], kwargs['dim_t']
+            )
+            cf.scroll_instruction_update(
+                kwargs['fuzz'], kwargs['dim_t']
+            )
+            kwargs['arr_t'] = cf.update_array_positions(
+                kwargs['tail_position'],
+                kwargs['buffer_length'],
+                kwargs['buffer_status'],
+                kwargs['buf_t'],
+                0
+            )
+            kwargs = self.prepare_frame():
 
-    def next_frame(self):
-        QCoreApplication.processEvents()
-        while time.time() - self.frametime < self.st.general.frametime:
-            QThread.msleep(1)
-        if QThread.currentThread().isInterruptionRequested():
-            self.error.emit('Interrupted')
-            self.finished.emit()
-            return
-        self.handlerSig.emit(self.frame)
-        self.frame = self.prepare_frame()
-        self.st.general.rundone += 1
-        self.frameSig.emit(self.st.general.rundone)
-        self.frametime = time.time()
+            self.fpsRoll[0] = time.time()-now
+            self.fpsRoll = np.roll(self.fpsRoll, 1)
+            self.arrayfpsSig.emit(np.mean(self.fpsRoll))
 
     def prepare_frame(self):
-        frame={
+        kwargs={
             'dim':self.st.canvas.dim,
             'threshold':self.st.noise.threshold,
-            'isingupdates':0,
+            'updates':self.st.ising.updates,
             'beta':self.st.ising.beta,
-            'conwayrules':[],
-            'bounds':[], #TODO:
-            'bars':[], #TODO:
-            'fuzz':[], #TODO:
-            }
-        frame['isingupdates'] = self.st.ising.updates * self.st.general.stochastic
-        frame['conwayrules'] = rules
+            'rules':self.st.conway.rules,
+            'bounds':np.asarray(self.st.bounds, np.intc),
+            'bars':np.asarray(self.bars, np.double),
+            'fuzz':np.asarray(self.fuzz, np.double),
+            'head_position':np.asarray(self.head_position),
+            'tail_position':np.asarray(self.tail_position),
+            'buffer_length':np.asarray(self.buf_len),
+            'buffer_status':np.asarray(self.buf_stat),
+            'dim':np.asarray(self.dim),
+            'arr_h':np.asarray(self.arr_h),
+            'buf':np.asarray(self.buf),
+            'arr_t':np.asarray(self.arr_t),
         return frame
-
-    def updater_start(self, frame1, frame2, dim):
-        if not self.array.shape == tuple(dim):
-            self.array = super().resize_array(dim)
-        self.array = super().process(frame1, self.array)
-        self.arrayinitSig.emit(self.array, frame1['wolfpos'], dim)
-        self.array = super().process(frame2, self.array)
-
-    #TODO: This is what the basic head neads to have in it
-    def next_array(self, frame):
-        now = time.time()
-        self.arraySig.emit(self.array, frame['wolfpos'], frame['dim'])
-        self.array = super().process(frame, self.array)
-        self.fpsRoll[0] = time.time()-now
-        self.fpsRoll = np.roll(self.fpsRoll, 1)
-        self.arrayfpsSig.emit(np.mean(self.fpsRoll))
-
-
-class Analyser(QObject, pureHandler):
-    popSig = pyqtSignal(np.ndarray, str)
-    radSig = pyqtSignal(np.ndarray, str)
-    nextarraySig = pyqtSignal()
-
-
-    def __init__(self, st):
-        QObject.__init__(self)
-
-        self.st = st
-        super().clear()
-
-    def process(self, array):
-        super().analyse(array)
-        center,population,radius = super().export()
-        self.popSig.emit(population, 'p1')
-        self.radSig.emit(radius, 'p2')
-        self.nextarraySig.emit()
-
-class ImageCreator(QObject, pureHandler):
-    imageSig = pyqtSignal(QPixmap)
-    analyseSig = pyqtSignal(np.ndarray)
-    breakSig = pyqtSignal()
-    error = pyqtSignal(str)
-    finished = pyqtSignal()
-    canvasfpsSig = pyqtSignal(float)
-
-    def __init__(self, st):
-        QObject.__init__(self)
-
-        self.error.emit('Image thread starting up!')
-        self.colorList = st.canvas.colorlist
-        self.wolfram_color_offset = 2
-        self.resize_image(st.canvas.dim)
-
-        self.scale = st.canvas.scale
-        self.fpsRoll = np.zeros(9, float)
-        self.st = st
-
-        self.savecount = 0
-
-    def reset_gifcount(self):
-        self.savecount = 0
 
     # Resize/reset
     # Image and array have the same size, should be resized in one function.
@@ -156,39 +139,35 @@ class ImageCreator(QObject, pureHandler):
         self.imageDim = dim
 
 #===============Array processing and Image export=============#
-    def send_image(self, image):
-        ims = image.scaled(QSize(self.st.canvas.dim[0] * self.st.canvas.scale,
+    def send_image(self):
+        ims = self.image.scaled(QSize(self.st.canvas.dim[0] * self.st.canvas.scale,
                                  self.st.canvas.dim[1] * self.st.canvas.scale))
         nupix = QPixmap()
         nupix.convertFromImage(ims)
         self.imageSig.emit(nupix)
-        if self.st.canvas.record:
-            ims.save('images/temp{:>04d}.png'.format(self.savecount), 'PNG')
-            self.savecount += 1
 
-    def processer_start(self, array, pos, dim):
-        self.resize_image(dim)
-        self.wavecounter = pos
-        super().make_wolf(True, dim, self.st.wolfram.scale, self.st.wolfram.rule)
-        super().save_array(array)
-        self.process_array(array)
-        self.analyseSig.emit(array)
+    #TODO: surely I can just use the array as an image directly? This is rediculous.
+    def export_array(self, A, color_offset):
+        """
+        Updates the image with the values from an entire array.
 
-    def process(self, array, pos, dim):
-        if not self.imageDim == dim:
-            self.resize_image(dim)
-        self.send_image(self.image)
-        self.analyseSig.emit(array)
-        self.wavecounter = pos
-        self.process_array(array)
+        :param self:
+        :param A:               Array to use as new image
+        :param color_offset:    Use primary colors (0) or secondary colors (2)?
+        :return:                None
+        """
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                num = int(A[i][j])
+                color = self.canvas.colorList[num + color_offset]
+                self.image.setPixel(i, j, color)
 
-    def process_array(self, array):
-        now = time.time()
-        change = super().change(array)
-        self.export_list(change)
-        if self.st.general.wolfwave:
-            self.wolfram_scroll()
-        super().save_array(array)
-        self.fpsRoll[0] = time.time()-now
-        self.fpsRoll = np.roll(self.fpsRoll, 1)
-        self.canvasfpsSig.emit(np.mean(self.fpsRoll))
+    def replace_image_positions(self, L, color):
+        """
+        Updates the given positions with the specified color
+
+        :param L:       List of positions to update
+        :param color:   selection from colorlist
+        :return:        None
+        """
+        [self.image.setPixel(el[0], el[1], self.canvas.colorList[color]) for el in L]
